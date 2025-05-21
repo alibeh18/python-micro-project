83dac97f2cb92c7eb1dd6e89502546e004f79a86
Alibeh18 <alibehrouzifh@gmail.com>
2025-05-21 20:15:29 +0330

    FEATURE : ADD SOME PYTHON MICRO PROJECT

---
+++ hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def hello(ali=1):
+    return(ali)
+saba = hello()
+
+print(saba)
---
+++ tavan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+squarts= [i ** 2 for i in range(10)]
+
+print(squarts)
+
+
+#squart = [ ]
+
+#for i in range(10):
+ #   squart.append(i ** 2)
+#ali = squart
+
+#print(ali)
